14ba4f08821efb32d0c483c1d3acbbd45c0a412a
MechEngMalik <mecheng.malik93@gmail.com>
2021-11-16 17:46:22 +0200

    finish-lab28

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -11,12 +13,27 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        List<Task> allTasksData = new ArrayList<>();
+        allTasksData.add(new Task("visiting a doctor","general routine examination","complete"));
+        allTasksData.add(new Task("go to bank","make a new account for bills","new"));
+        allTasksData.add(new Task("pay the bills","Pay monthly bills","in progress"));
+
+        RecyclerView allTaskRecyclerView = findViewById(R.id.taskRecyclerView);
+        allTaskRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        allTaskRecyclerView.setAdapter(new TaskAdapter(allTasksData));
+
+
 
 
         Button addTask = findViewById(R.id.addTask);
@@ -39,39 +56,45 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        Button task1 = findViewById(R.id.task1);
-        task1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String visitADoc = task1.getText().toString();
-                Intent intentTask1 = new Intent(MainActivity.this, Task_Detail.class);
-                intentTask1.putExtra("title", visitADoc);
-                startActivity(intentTask1);
-            }
-        });
 
 
-        Button task2 = findViewById(R.id.task2);
-        task2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String goToBank = task2.getText().toString();
-                Intent intentTask2 = new Intent(MainActivity.this, Task_Detail.class);
-                intentTask2.putExtra("title", goToBank);
-                startActivity(intentTask2);
-            }
-        });
 
-        Button task3 = findViewById(R.id.task3);
-        task3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String payBells = task3.getText().toString();
-                Intent intentTask3 = new Intent(MainActivity.this, Task_Detail.class);
-                intentTask3.putExtra("title", payBells);
-                startActivity(intentTask3);
-            }
-        });
+        //---------------------------------------------------------------------
+
+
+//        Button task1 = findViewById(R.id.task1);
+//        task1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String visitADoc = task1.getText().toString();
+//                Intent intentTask1 = new Intent(MainActivity.this, Task_Detail.class);
+//                intentTask1.putExtra("title", visitADoc);
+//                startActivity(intentTask1);
+//            }
+//        });
+//
+//
+//        Button task2 = findViewById(R.id.task2);
+//        task2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String goToBank = task2.getText().toString();
+//                Intent intentTask2 = new Intent(MainActivity.this, Task_Detail.class);
+//                intentTask2.putExtra("title", goToBank);
+//                startActivity(intentTask2);
+//            }
+//        });
+//
+//        Button task3 = findViewById(R.id.task3);
+//        task3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String payBells = task3.getText().toString();
+//                Intent intentTask3 = new Intent(MainActivity.this, Task_Detail.class);
+//                intentTask3.putExtra("title", payBells);
+//                startActivity(intentTask3);
+//            }
+//        });
 
 
         Button sittings = findViewById(R.id.sittingsButton);
